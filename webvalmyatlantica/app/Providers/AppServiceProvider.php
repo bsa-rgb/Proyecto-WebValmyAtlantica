5612f56d2ccff69207274d8f05ec06a4b7ef56ba
bea <bsa698x@gmail.com>
2026-02-26 17:59:55 +0100

    Cambio de idioma de Faker a es_ES

--- webvalmyatlantica/app/Providers/AppServiceProvider.php
+++ webvalmyatlantica/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Faker\Factory as Faker;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        $this->app->singleton(\Faker\Generator::class, function () {
+        return Faker::create('es_ES');
+        });
     }
 }
