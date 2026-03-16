9ee9cafa28f1bec6919b0edff98d121fa371b388
bea <bsa698x@gmail.com>
2026-03-16 11:59:25 +0100

    Reorganizar archivo para landing - index

---
+++ webvalmyatlantica/resources/js/Pages/Public/Landing.vue
@@ -0,0 +1,9 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+</script>
+
+<template>
+    <GuestLayout>
+        <h1>Landing Valmy Atlántica</h1>
+    </GuestLayout>
+</template>
