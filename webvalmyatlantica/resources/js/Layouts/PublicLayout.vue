bb4defac9ac472b35144486cb1443083c2ca6353
bea <bsa698x@gmail.com>
2026-03-16 17:37:18 +0100

    importar y organizar archivos para el layout

---
+++ webvalmyatlantica/resources/js/Layouts/PublicLayout.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import Navbar from '@/Components/Navbar.vue';
+import Footer from '@/Components/Footer.vue';
+import Navbar from '@/Components/Navbar.vue';
+import Footer from '@/Components/Footer.vue';
+
+</script>
+
+<template>
+
+    <Navbar />
+
+    <main>
+        <slot />
+    </main>
+
+    <Footer />
+    
+</template>
