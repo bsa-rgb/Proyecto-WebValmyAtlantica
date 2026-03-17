9cfb5cf169ab93d931f7c322995d1ac4a63dd19e
bea <bsa698x@gmail.com>
2026-03-17 23:42:04 +0100

    Empezar diseño seccion Stats

--- webvalmyatlantica/resources/js/Components/StatsSection.vue
+++ webvalmyatlantica/resources/js/Components/StatsSection.vue
@@ -1,5 +1,27 @@
+<script setup>
+</script>
+
 <template>
-  <section class="stats-section">
-    <p>Stats Section</p>
+  <section class="stats">
+
+    <div class="stats-container">
+
+      <div class="stat">
+        <h2>25+</h2>
+        <p>Años de experiencia</p>
+      </div>
+
+      <div class="stat">
+        <h2>500+</h2>
+        <p>Empresas asesoradas</p>
+      </div>
+
+      <div class="stat">
+        <h2>1.2M€</h2>
+        <p>Ahorro energético</p>
+      </div>
+
+    </div>
+
   </section>
 </template>
