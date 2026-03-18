ed9c60dc90ec0410313e9b9ee454351091c70fec
bea <bsa698x@gmail.com>
2026-03-18 16:09:13 +0100

    Diseñar - provisional - sección Clientes

--- webvalmyatlantica/resources/js/Components/ClientsSection.vue
+++ webvalmyatlantica/resources/js/Components/ClientsSection.vue
@@ -1,5 +1,38 @@
+<script setup>
+</script>
+
 <template>
-  <section class="clients-section">
-    <p>Clients Section</p>
+  <section class="clients">
+
+    <div class="clients-container">
+
+      <p class="clients-title">Algunas de las empresas que confían en nosotros</p>
+
+      <!--PRUEBA-->
+      <div class="clients-logos">
+        <div class="logos-track">
+          <!-- Set original -->
+          <div class="client-logo"></div>
+          <div class="client-logo"></div>
+          <div class="client-logo"></div>
+          <div class="client-logo"></div>
+          <div class="client-logo"></div>
+          <div class="client-logo"></div>
+          <div class="client-logo"></div>
+          <div class="client-logo"></div>
+          <!-- Set duplicado (copia exacta para el loop infinito) -->
+          <div class="client-logo"></div>
+          <div class="client-logo"></div>
+          <div class="client-logo"></div>
+          <div class="client-logo"></div>
+          <div class="client-logo"></div>
+          <div class="client-logo"></div>
+          <div class="client-logo"></div>
+          <div class="client-logo"></div>
+        </div>
+      </div>
+
+    </div>
+
   </section>
 </template>
