2fa9a91f0170127e4df559eca159588bcab74622
bea <bsa698x@gmail.com>
2026-03-18 17:59:37 +0100

    Diseñar sección CTA

--- webvalmyatlantica/resources/js/Components/CTASection.vue
+++ webvalmyatlantica/resources/js/Components/CTASection.vue
@@ -1,5 +1,21 @@
+<script setup>
+</script>
+
 <template>
-  <section class="cta-section">
-    <p>CTA Section</p>
+
+  <section class="cta">
+
+    <div class="cta-container">
+
+      <h2>Empiece a optimizar su energía hoy</h2>
+      <p>Analizamos su situación y le ayudamos a tomar decisiones energéticas con confianza.</p>
+
+      <button class="button-primary">
+        Contactar ahora
+      </button>
+
+    </div>
+
   </section>
+
 </template>
