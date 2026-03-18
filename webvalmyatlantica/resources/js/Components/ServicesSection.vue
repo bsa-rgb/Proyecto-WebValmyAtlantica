638cbb58d383b0494a622f7beb35a8f2d8acb22e
bea <bsa698x@gmail.com>
2026-03-18 12:19:42 +0100

    Diseñar sección Servicios

--- webvalmyatlantica/resources/js/Components/ServicesSection.vue
+++ webvalmyatlantica/resources/js/Components/ServicesSection.vue
@@ -1,5 +1,40 @@
+<script setup>
+</script>
+
 <template>
-  <section class="services-section">
-    <p>Services Section</p>
+  <section class="services">
+
+    <div class="service-block">
+      <div class="service-image"></div>
+      <div class="service-text">
+        <h3>La energía también se decide</h3>
+        <p>Las decisiones sobre energía influyen directamente en el rumbo de una empresa.</p>
+        <button class="button-primary">Saber más →</button>
+      </div>
+    </div>
+
+    <div class="service-block reverse">
+      <div class="service-image"></div>
+      <div class="service-text">
+        <h3>Experiencia energética al lado de las empresas</h3>
+        <p>Más de dos décadas trabajando en el sector energético junto a empresas de distintos sectores productivos.</p>
+        <button class="button-primary">Saber más →</button>
+      </div>
+    </div>
+
+    <div class="service-block">
+      <div class="service-image"></div>
+      <div class="service-text">
+        <h3>Comprender. Interpretar. Acompañar.</h3>
+        <p>Analizamos cada situación energética y acompañamos a las empresas cuando tienen que tomar decisiones.</p>
+        <button class="button-primary">Saber más →</button>
+      </div>
+    </div>
+
+      <div class="services-end">
+      <h2>El sector energético seguirá cambiando. Eso es seguro.</h2>
+      <p>Las empresas necesitan comprender su entorno energético para tomar decisiones con mayor seguridad. En Valmy Atlántica ayudamos a entender esa realidad con conocimiento, experiencia y un compromiso cercano cuando más se necesita.</p>
+    </div>
+    
   </section>
 </template>
