828fb25088287aeeab67a3216d6215f190bf22a1
bea <bsa698x@gmail.com>
2026-03-20 06:01:46 +0100

    Modificar nombre e icono en pestaña de navegador

--- webvalmyatlantica/resources/views/app.blade.php
+++ webvalmyatlantica/resources/views/app.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title>Valmy Atlántica</title>
+        <link rel="icon" type="image/svg+xml" href="/images/favicon.png">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
