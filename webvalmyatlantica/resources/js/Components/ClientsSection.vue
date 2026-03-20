24b98f6c319ef23ca871d507785530ede058167a
bea <bsa698x@gmail.com>
2026-03-20 06:05:04 +0100

    Formatear bonito - prettier

--- webvalmyatlantica/resources/js/Components/ClientsSection.vue
+++ webvalmyatlantica/resources/js/Components/ClientsSection.vue
@@ -1,80 +1,97 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
   <section class="clients">
-
     <div class="clients-container">
-
-      <p class="clients-title">Algunas de las empresas que confían en nosotros</p>
+      <p class="clients-title">
+        Algunas de las empresas que confían en nosotros
+      </p>
 
       <div class="clients-logos">
         <div class="logos-track">
           <div class="client-logo">
-            <img src="/images/vg.jpg" alt="logo Voz de Galicia" />
+            <a href="https://www.lavozdegalicia.es" target="_blank" rel="noopener">
+              <img src="/images/vg.jpg" alt="logo Voz de Galicia" />
+            </a>
           </div>
           <div class="client-logo">
-            <img src="/images/Aenor.jpg" alt="logo Aenor" />
+            <a href="https://www.aenor.com/" target="_blank" rel="noopener">
+              <img src="/images/Aenor.jpg" alt="logo Aenor" />
+            </a>
           </div>
           <div class="client-logo">
-            
-            <img src="/images/cambalache.jpg" alt="logo Cambalache" />
+            <a href="https://restaurantescambalache.es/" target="_blank" rel="noopener">
+              <img src="/images/cambalache.jpg" alt="logo Cambalache" />
+            </a>
           </div>
           <div class="client-logo">
-            
-            <img src="/images/Clavo.jpg" alt="logo Clavo" />
+            <a href="https://www.clavofoodfactory.com/es/inicio/" target="_blank" rel="noopener">
+              <img src="/images/Clavo.jpg" alt="logo Clavo" />
+            </a>
           </div>
           <div class="client-logo">
-            
-            <img src="/images/Clun.jpg" alt="logo Clun" />
+            <a href="https://clun.es/es/" target="_blank" rel="noopener">
+              <img src="/images/Clun.jpg" alt="logo Clun" />
+            </a>
           </div>
           <div class="client-logo">
-            
-            <img src="/images/Cogami.jpg" alt="logo Cogami" />
+            <a href="https://www.cogami.gal/" target="_blank" rel="noopener">
+              <img src="/images/Cogami.jpg" alt="logo Cogami" />
+            </a>
           </div>
           <div class="client-logo">
-            
-            <img src="/images/edo.jpg" alt="logo Edo" />
+            <a href="https://www.espinaydelfin.com/" target="_blank" rel="noopener">
+              <img src="/images/edo.jpg" alt="logo Edo" />
+            </a>
           </div>
           <div class="client-logo">
-            
-            <img src="/images/Isabel.jpg" alt="logo Isabel" />
+            <a href="https://www.isabel.net/" target="_blank" rel="noopener">
+              <img src="/images/Isabel.jpg" alt="logo Isabel" />
+            </a>
           </div>
-          
+
           <div class="client-logo">
-            <img src="/images/vg.jpg" alt="logo Voz de Galicia" />
-          </div>
-          <div class="client-logo">
-            <img src="/images/Aenor.jpg" alt="logo Aenor" />
-          </div>
-          <div class="client-logo">
-            
-            <img src="/images/cambalache.jpg" alt="logo Cambalache" />
+            <a href="https://www.lavozdegalicia.es" target="_blank" rel="noopener">
+              <img src="/images/vg.jpg" alt="logo Voz de Galicia" />
+            </a>
           </div>
           <div class="client-logo">
-            
-            <img src="/images/Clavo.jpg" alt="logo Clavo" />
+            <a href="https://www.aenor.com/" target="_blank" rel="noopener">
+              <img src="/images/Aenor.jpg" alt="logo Aenor" />
+            </a>
           </div>
           <div class="client-logo">
-            
-            <img src="/images/Clun.jpg" alt="logo Clun" />
+            <a href="https://restaurantescambalache.es/" target="_blank" rel="noopener">
+              <img src="/images/cambalache.jpg" alt="logo Cambalache" />
+            </a>
           </div>
           <div class="client-logo">
-            
-            <img src="/images/Cogami.jpg" alt="logo Cogami" />
+            <a href="https://www.clavofoodfactory.com/es/inicio/" target="_blank" rel="noopener">
+              <img src="/images/Clavo.jpg" alt="logo Clavo" />
+            </a>
           </div>
           <div class="client-logo">
-            
-            <img src="/images/edo.jpg" alt="logo Edo" />
+            <a href="https://clun.es/es/" target="_blank" rel="noopener">
+              <img src="/images/Clun.jpg" alt="logo Clun" />
+            </a>
           </div>
           <div class="client-logo">
-            
-            <img src="/images/Isabel.jpg" alt="logo Isabel" />
+            <a href="https://www.cogami.gal/" target="_blank" rel="noopener">
+              <img src="/images/Cogami.jpg" alt="logo Cogami" />
+            </a>
+          </div>
+          <div class="client-logo">
+            <a href="https://www.espinaydelfin.com/" target="_blank" rel="noopener">
+              <img src="/images/edo.jpg" alt="logo Edo" />
+            </a>
+          </div>
+          <div class="client-logo">
+            <a href="https://www.isabel.net/" target="_blank" rel="noopener">
+              <img src="/images/Isabel.jpg" alt="logo Isabel" />
+            </a>
           </div>
         </div>
       </div>
-
     </div>
-
   </section>
 </template>
